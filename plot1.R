42aeb48484a0fcbff3fc14ca87d76a32124274cb
Glenn (Ted) Dunmire <glenn.dunmire.iv@gmail.com>
2014-06-06 19:58:16 -0400

    Adding documentation to plot1 and plot 2, adding plot3

--- plot1.R
+++ plot1.R
@@ -1,10 +1,24 @@
-data <- read.table("./exdata_data_household_power_consumption/household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?", stringsAsFactors = FALSE)
+##Plot 1 of Course Project 1 for Exploratory Data Analysis, completed by Glenn Dunmire on 6/7/2014
+##This script assumes the person running it has navigated to their working directory and placed a folder
+##called "exdata_data_household_power_consumption in their working directory, obtained from the UCI Machine
+##Learning database: https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+##This file is then unzipped, but no other modifications are made. 
+
+##This plots a histogram showing the frequency of values for the Global Active Power measurements
+
+##Read the data, note sep = ";" and na.strings = "?" and stringsAsFactors set to FALSE
+data <- read.table("./exdata_data_household_power_consumption/household_power_consumption.txt", 
+                   header = TRUE, sep = ";", na.strings = "?", stringsAsFactors = FALSE)
 
 
+##Coerce the characters in the Date column to class Date
 data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
 
+##subset data to get relevant dates
 datasub <- subset(data, data$Date == "2007-02-01" | data$Date == "2007-02-02")
 
+##create a png file called plot1.png that displays a histogram of the Global Active Power for Feb 1 and 2 in 2007
+##This plot will output in the working directory. 
 png(filename = "plot1.png", width = 480, height = 480, bg = "transparent")
 hist(datasub$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red")
 dev.off()
--- plot3.R
+++ plot3.R
@@ -1,27 +1,36 @@
-##Plot 2 of Course Project 1 for Exploratory Data Analysis, completed by Glenn Dunmire on 6/7/2014
+##Plot 3 of Course Project 1 for Exploratory Data Analysis, completed by Glenn Dunmire on 6/7/2014
 ##This script assumes the person running it has navigated to their working directory and placed a folder
 ##called "exdata_data_household_power_consumption in their working directory, obtained from the UCI Machine
 ##Learning database: https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
 ##This file is then unzipped, but no other modifications are made. 
 
-##This plots a line showing the Global Active Power measured over the two days
+##This plots the three Sub_metering variables as lines to compare their values. 
 
 #read data
 data <- read.table("./exdata_data_household_power_consumption/household_power_consumption.txt", 
                    header = TRUE, sep = ";", na.strings = "?", stringsAsFactors = FALSE)
 
-#coerce data$Date column from character to a Date
+#Coerce characters in Date to Date class
 data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
 
-#Subset dataset for relevant dates
+#subset relevant dates
 datasub <- subset(data, data$Date == "2007-02-01" | data$Date == "2007-02-02")
 
 #Using paste, create a character vector combining the Date and Time columns. Then pass to the strptime() function
 #using appropriate format to result in POSIXlt and POSIXt class objects. Store as a new column called datetime
 datasub$datetime <- strptime(paste(datasub$Date, datasub$Time), format = "%Y-%m-%d %H:%M:%S")
 
-##produce appropriate png file in working directory
-png(filename = "plot2.png", width = 480, height = 480, bg = "transparent")
-plot(x = datasub$datetime, y = datasub$Global_active_power, xlab = "", 
-     ylab = "Global Active Power (kilowatts)", main = "", type = "l")
+#produce png as plot3.png in working directory
+png(filename = "plot3.png", width = 480, height = 480, bg = "transparent")
+#plots initial Sub_metering_1
+plot(x = datasub$datetime, y = datasub$Sub_metering_1, xlab = "", ylab = "Energy sub metering", type = "l")
+
+#add Sub_metering_2
+lines(x = datasub$datetime, y = datasub$Sub_metering_2, col = "red")
+#add Sub_metering_3
+lines(x = datasub$datetime, y = datasub$Sub_metering_3, col = "blue")
+
+#add a legend to identify which color line is which
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       col = c("black", "red", "blue"), lty = c(1, 1, 1))
 dev.off()
